b1f1b015d185f1702fcf8f1fdfbb9e6021266288
Darren Reid <darren.j.reid@gmail.com>
2016-02-03 09:41:01 +1100

    Create travis yml

--- build.sh
+++ build.sh
@@ -4,5 +4,4 @@ cp pp_project_gapublicvocabssandbox.rdf indexing/resources/rdfdata/pp_project_ga
 rm indexing/config/indexing.properties
 cp indexing.properties indexing/config/indexing.properties
 java -jar -Xmx1g -XX:MaxPermSize=256M /tools/org.apache.stanbol.entityhub.indexing.genericrdf-0.12.1-SNAPSHOT.jar index
-# Run it twice because first time fails on method not found...
 java -jar -Xmx1g -XX:MaxPermSize=256M /tools/org.apache.stanbol.entityhub.indexing.genericrdf-0.12.1-SNAPSHOT.jar index
