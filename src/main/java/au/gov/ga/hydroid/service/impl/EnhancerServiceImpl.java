cd80613c994c35d41e701fd3268bf87af71ce27b
scelton <elton.carneiro@ga.gov.au>
2016-02-04 17:11:32 +1100

    Tied-up enhancer service

--- src/main/java/au/gov/ga/hydroid/service/impl/EnhancerServiceImpl.java
+++ src/main/java/au/gov/ga/hydroid/service/impl/EnhancerServiceImpl.java
@@ -31,52 +31,59 @@ public class EnhancerServiceImpl implements EnhancerService {
    @Autowired
    private DocumentService documentService;
 
+   private Properties generateSolrDocument(List<Statement> rdfDocument, String content) {
+      String predicate = null;
+      String currentSubject = null;
+      boolean isValidEntityRef = true;
+      StringBuilder concept = new StringBuilder();
+      StringBuilder label = new StringBuilder();
+      Properties properties = new Properties();
+      for (Statement statement : rdfDocument) {
+         predicate = statement.getPredicate().getLocalName().toLowerCase();
+         if (!statement.getSubject().stringValue().equalsIgnoreCase(currentSubject)) {
+            currentSubject = statement.getSubject().stringValue();
+            //isValidEntityRef = false;
+         }
+         if (ArrayUtils.indexOf(VALID_PREDICATES, predicate) >= 0) {
+            if (predicate.equalsIgnoreCase("extracted-from") && properties.getProperty("about") == null) {
+               properties.put("about", statement.getObject().stringValue());
+            } else if (predicate.equalsIgnoreCase("entity-reference")) {
+               isValidEntityRef = (statement.getObject().stringValue().indexOf(GA_PUBLIC_VOCABS) >= 0);
+               // add new concept if not there yet
+               if (isValidEntityRef && concept.indexOf(predicate) < 0) {
+                  concept.append(statement.getObject().stringValue()).append(", ");
+               }
+            } else if (predicate.equalsIgnoreCase("entity-label")) {
+               // add new label if not there yet
+               if (isValidEntityRef && label.indexOf(predicate) < 0) {
+                  label.append(statement.getObject().stringValue()).append(", ");
+               }
+            } else {
+               properties.put(predicate, statement.getObject().stringValue());
+            }
+         }
+      }
+
+      String solrContent = content;
+      if (solrContent.length() > 500) {
+         solrContent = solrContent.substring(0, 500) + "...";
+      }
+      properties.put("content", solrContent);
+      properties.put("label", label.toString());
+      properties.put("concept", concept.toString());
+      properties.put("creator", "Hydroid Enhancer App");
+
+      return properties;
+   }
+
    @Override
    public void enhance(String chainName, String content, String solrCollection) throws Exception {
 
       // Send content to Stanbol for enhancement
       List<Statement> rdfDocument = stanbolClient.enhance(chainName, content, StanbolMediaTypes.RDFXML);
       if (rdfDocument != null) {
-         String predicate = null;
-         String currentSubject = null;
-         boolean isValidEntityRef = true;
-         StringBuilder concept = new StringBuilder();
-         StringBuilder label = new StringBuilder();
-         Properties properties = new Properties();
-         for (Statement statement : rdfDocument) {
-            predicate = statement.getPredicate().getLocalName().toLowerCase();
-            if (!statement.getSubject().stringValue().equalsIgnoreCase(currentSubject)) {
-               currentSubject = statement.getSubject().stringValue();
-               //isValidEntityRef = false;
-            }
-            if (ArrayUtils.indexOf(VALID_PREDICATES, predicate) >= 0) {
-               if (predicate.equalsIgnoreCase("extracted-from") && properties.getProperty("about") == null) {
-                  properties.put("about", statement.getObject().stringValue());
-               } else if (predicate.equalsIgnoreCase("entity-reference")) {
-                  isValidEntityRef = (statement.getObject().stringValue().indexOf(GA_PUBLIC_VOCABS) >= 0);
-                  // add new concept if not there yet
-                  if (isValidEntityRef && concept.indexOf(predicate) < 0) {
-                     concept.append(statement.getObject().stringValue()).append(", ");
-                  }
-               } else if (predicate.equalsIgnoreCase("entity-label")) {
-                  // add new label if not there yet
-                  if (isValidEntityRef && label.indexOf(predicate) < 0) {
-                     label.append(statement.getObject().stringValue()).append(", ");
-                  }
-               } else {
-                  properties.put(predicate, statement.getObject().stringValue());
-               }
-            }
-         }
-
-         String solrContent = content;
-         if (solrContent.length() > 500) {
-            solrContent = solrContent.substring(0, 500) + "...";
-         }
-         properties.put("content", solrContent);
-         properties.put("label", label.toString());
-         properties.put("concept", concept.toString());
-         properties.put("creator", "Hydroid Enhancer App");
+         // Generate dictionary with properties we are interested in
+         Properties properties = generateSolrDocument(rdfDocument, content);
 
          // Add enhanced document to Solr
          solrClient.addDocument("hydroid", properties);
@@ -88,6 +95,8 @@ public class EnhancerServiceImpl implements EnhancerService {
          documentService.create(document);
 
          //todo if database operation failed, need to remove doc from Solr
+      } else {
+         // todo error handling here as well
       }
 
    }
