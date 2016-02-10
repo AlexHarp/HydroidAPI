12c0a10ec918e45c89d8fa50fc00336fe347e373
Darren Reid <darren.j.reid@gmail.com>
2016-02-10 15:14:58 +1100

    Moved to use Apache Tika for generic text extract

--- src/main/java/au/gov/ga/hydroid/controller/FileIndexController.java
+++ src/main/java/au/gov/ga/hydroid/controller/FileIndexController.java
@@ -3,8 +3,11 @@ package au.gov.ga.hydroid.controller;
 import au.gov.ga.hydroid.HydroidConfiguration;
 import au.gov.ga.hydroid.dto.ServiceResponse;
 import au.gov.ga.hydroid.service.EnhancerService;
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.util.PDFTextStripper;
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.mime.MediaType;
+import org.apache.tika.parser.AutoDetectParser;
+import org.apache.tika.sax.BodyContentHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,8 +16,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.multipart.MultipartFile;
+import org.xml.sax.SAXException;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Created by Layoric on 9/02/2016.
@@ -37,10 +43,8 @@ public class FileIndexController {
         if (!file.isEmpty()) {
             try {
                 byte[] bytes = file.getBytes();
-                PDFTextStripper pdfTextStripper = new PDFTextStripper();
                 ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
-                PDDocument doc = PDDocument.load(byteArrayInputStream);
-                String text = pdfTextStripper.getText(doc);
+                String text = parseFile(byteArrayInputStream);
                 enhancerService.enhance(configuration.getStanbolChain(),
                         name,
                         text,
@@ -55,5 +59,13 @@ public class FileIndexController {
 
         return new ResponseEntity<ServiceResponse>(new ServiceResponse("Your document has been enhanced successfully."),
                 HttpStatus.OK);
+    }
+
+    public String parseFile(InputStream stream) throws IOException, SAXException, TikaException {
+        AutoDetectParser parser = new AutoDetectParser();
+        BodyContentHandler handler = new BodyContentHandler();
+        Metadata metadata = new Metadata();
+        parser.parse(stream, handler, metadata);
+        return handler.toString();
     }
 }
