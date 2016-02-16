33d9c1923c5f623ce478e57753858dc870f81041
scelton <elton.carneiro@ga.gov.au>
2016-02-16 15:53:20 +1100

    Fixed the create function in DocumentService

--- src/main/java/au/gov/ga/hydroid/service/impl/DocumentServiceImpl.java
+++ src/main/java/au/gov/ga/hydroid/service/impl/DocumentServiceImpl.java
@@ -3,11 +3,13 @@ package au.gov.ga.hydroid.service.impl;
 import au.gov.ga.hydroid.model.Document;
 import au.gov.ga.hydroid.model.DocumentRowMapper;
 import au.gov.ga.hydroid.service.DocumentService;
+import org.openrdf.model.Statement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.PreparedStatement;
 import java.util.List;
 
 /**
@@ -34,7 +36,7 @@ public class DocumentServiceImpl implements DocumentService {
    @Transactional
    public void create(Document document) {
       jdbcTemplate.update("insert into documents (urn, title, type, content) values (?, ?, ?, ?)",
-            document.getUrn(), document.getContent());
+            document.getUrn(), document.getTitle(), document.getType().name(), document.getContent());
    }
 
    @Override
