54afc7b961d2f280b11221c05d7f6ae0a1ce24b9
Darren Reid <darren.reid@ga.gov.au>
2016-02-23 15:43:47 +1100

    Add test

--- src/test/java/au/gov/ga/hydroid/EnhancerServiceTestIT.java
+++ src/test/java/au/gov/ga/hydroid/EnhancerServiceTestIT.java
@@ -1,12 +1,23 @@
 package au.gov.ga.hydroid;
 
+import au.gov.ga.hydroid.controller.FileIndexController;
+import au.gov.ga.hydroid.model.DocumentType;
 import au.gov.ga.hydroid.service.EnhancerService;
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.AutoDetectParser;
+import org.apache.tika.sax.BodyContentHandler;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Created by u24529 on 3/02/2016.
@@ -25,17 +36,24 @@ public class EnhancerServiceTestIT {
    @Test
    public void testEnhance() throws Exception {
       String randomText = "Created: " + System.currentTimeMillis();
+      String text = parseFile(this.getClass().getResourceAsStream("/testfiles/36_4_1175-1197_Buss_and_Clote.pdf"));
+
       enhancerService.enhance("Document Title" + randomText,
-            "The polar bear (Ursus maritimus) is a carnivorous bear whose native range lies largely within the Arctic Circle, encompassing the Arctic Ocean, its surrounding seas and surrounding land masses. It is a large bear, approximately the same size as the omnivorous Kodiak bear (Ursus arctos middendorffi).[3] A boar (adult male) weighs around 350–700 kg (772–1,543 lb),[4] while a sow (adult female) is about half that size. Although it is the sister species of the brown bear,[5] it has evolved to occupy a narrower ecological niche, with many body characteristics adapted for cold temperatures, for moving across snow, ice, and open water, and for hunting seals, which make up most of its diet.[6] Although most polar bears are born on land, they spend most of their time on the sea ice. Their scientific name means \"maritime bear\", and derives from this fact. Polar bears hunt their preferred food of seals from the edge of sea ice, often living off fat reserves when no sea ice is present. Because of their dependence on the sea ice, polar bears are classified as marine mammals.[7]\n" +
-                  "\n" +
-                  "Because of expected habitat loss caused by climate change, the polar bear is classified as a vulnerable species, and at least three of the nineteen polar bear subpopulations are currently in decline.[8] For decades, large-scale hunting raised international concern for the future of the species but populations rebounded after controls and quotas began to take effect.[9] For thousands of years, the polar bear has been a key figure in the material, spiritual, and cultural life of Arctic indigenous peoples, and polar bears remain important in their cultures." +
-                  "\n" + randomText,
-            "document");
+              text,
+              DocumentType.DOCUMENT.name());
    }
 
    @Test
    public void testReindexDocument() throws Exception {
       enhancerService.reindexDocument("urn:content-item-sha1-88d676ed33e1b645fbd1e1a812f78b514ada8b16", false);
+   }
+
+   public static String parseFile(InputStream stream) throws IOException, SAXException, TikaException {
+      AutoDetectParser parser = new AutoDetectParser();
+      BodyContentHandler handler = new BodyContentHandler();
+      Metadata metadata = new Metadata();
+      parser.parse(stream, handler, metadata);
+      return handler.toString();
    }
 
 }
