ddc752cbe2706baf1a9c859db2d0faefba303506
Darren Reid <darren.reid@ga.gov.au>
2016-02-29 16:18:08 +1100

    More logging, fix proxy creation.

--- src/main/java/au/gov/ga/hydroid/service/impl/GoogleVisionImageService.java
+++ src/main/java/au/gov/ga/hydroid/service/impl/GoogleVisionImageService.java
@@ -2,6 +2,8 @@ package au.gov.ga.hydroid.service.impl;
 
 import au.gov.ga.hydroid.HydroidConfiguration;
 import au.gov.ga.hydroid.service.ImageService;
+import au.gov.ga.hydroid.utils.HydroidException;
+import com.drew.lang.annotations.NotNull;
 import com.google.api.client.googleapis.GoogleUtils;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
@@ -11,10 +13,12 @@ import com.google.api.services.vision.v1.Vision;
 import com.google.api.services.vision.v1.VisionRequestInitializer;
 import com.google.api.services.vision.v1.model.*;
 import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Nonnull;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.InetSocketAddress;
@@ -24,6 +28,8 @@ import java.util.ArrayList;
 
 @Service
 public class GoogleVisionImageService implements ImageService {
+
+   private Logger logger = LoggerFactory.getLogger(getClass());
 
    @Autowired
    private HydroidConfiguration configuration;
@@ -35,78 +41,76 @@ public class GoogleVisionImageService implements ImageService {
 
    @Override
    public String getImageMetadata(InputStream is) {
-      JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
-      Vision.Builder builder = null;
-      try {
-         builder = new Vision.Builder(newProxyTransport(configuration), jsonFactory, null);
-      } catch (GeneralSecurityException | IOException e) {
-         e.printStackTrace();
+      if(is == null) {
+         logger.debug("GoogleVisionImageService:getImageMetadata - input stream is null.");
+         throw new HydroidException("input stream is null");
       }
-      assert builder != null;
-      builder.setVisionRequestInitializer(new
-              VisionRequestInitializer(configuration.getGoogleVisionApiKey()));
-      Vision vision = builder.build();
+      try {
+         JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+         Vision.Builder builder;
 
-      BatchAnnotateImagesRequest batchAnnotateImagesRequest =
-              new BatchAnnotateImagesRequest();
-      batchAnnotateImagesRequest.setRequests(new ArrayList<AnnotateImageRequest>() {{
-         AnnotateImageRequest annotateImageRequest = new AnnotateImageRequest();
+         builder = new Vision.Builder(newProxyTransport(configuration), jsonFactory, null);
 
-         Image base64EncodedImage = new Image();
-
-         byte[] bytes = new byte[0];
-         try {
-            bytes = IOUtils.toByteArray(is);
-         } catch (IOException e) {
-            e.printStackTrace();
+         String apiKey = configuration.getGoogleVisionApiKey();
+         if(apiKey == null || apiKey.length() == 0) {
+            throw new HydroidException("Google Vision API key missing from configuration. Check ${google.vision.apiKey}.");
          }
-         base64EncodedImage.encodeContent(bytes);
-         annotateImageRequest.setImage(base64EncodedImage);
+         builder.setVisionRequestInitializer(new
+                 VisionRequestInitializer());
+         Vision vision = builder.build();
 
-         // add the features we want
-         annotateImageRequest.setFeatures(new ArrayList<Feature>() {{
-            Feature labelDetection = new Feature();
-            labelDetection.setType("LABEL_DETECTION");
-            labelDetection.setMaxResults(10);
-            add(labelDetection);
+         BatchAnnotateImagesRequest batchAnnotateImagesRequest =
+                 new BatchAnnotateImagesRequest();
+         batchAnnotateImagesRequest.setRequests(new ArrayList<AnnotateImageRequest>() {{
+            AnnotateImageRequest annotateImageRequest = new AnnotateImageRequest();
+
+            Image base64EncodedImage = new Image();
+
+            byte[] bytes = new byte[0];
+            bytes = IOUtils.toByteArray(is);
+
+            base64EncodedImage.encodeContent(bytes);
+            annotateImageRequest.setImage(base64EncodedImage);
+
+            // add the features we want
+            annotateImageRequest.setFeatures(new ArrayList<Feature>() {{
+               Feature labelDetection = new Feature();
+               labelDetection.setType("LABEL_DETECTION");
+               labelDetection.setMaxResults(10);
+               add(labelDetection);
+            }});
+
+            // Add the list of one thing to the request
+            add(annotateImageRequest);
          }});
 
-         // Add the list of one thing to the request
-         add(annotateImageRequest);
-      }});
+         Vision.Images.Annotate annotateRequest = vision.images().annotate(batchAnnotateImagesRequest);
 
-      Vision.Images.Annotate annotateRequest =
-              null;
-      try {
-         annotateRequest = vision.images().annotate(batchAnnotateImagesRequest);
-      } catch (IOException e) {
-         e.printStackTrace();
-      }
-      // Due to a bug: requests to Vision API containing large images fail when GZipped.
-      assert annotateRequest != null;
-      annotateRequest.setDisableGZipContent(true);
+         // Due to a bug: requests to Vision API containing large images fail when GZipped.
+         annotateRequest.setDisableGZipContent(true);
 
-      BatchAnnotateImagesResponse response = null;
-      try {
+         BatchAnnotateImagesResponse response;
+
          response = annotateRequest.execute();
-      } catch (IOException e) {
-         e.printStackTrace();
-      }
-      StringBuilder result = new StringBuilder();
-      assert response != null;
-      for(AnnotateImageResponse imgRes : response.getResponses()) {
-         for(EntityAnnotation entityAnnotation : imgRes.getLabelAnnotations()) {
-            result.append(entityAnnotation.getDescription()).append(",");
+
+         StringBuilder result = new StringBuilder();
+         for (AnnotateImageResponse imgRes : response.getResponses()) {
+            for (EntityAnnotation entityAnnotation : imgRes.getLabelAnnotations()) {
+               result.append(entityAnnotation.getDescription()).append(",");
+            }
          }
+         return result.substring(0, result.length() - 1);
       }
-      return result.substring(0,result.length() - 1);
+      catch (Throwable e) {
+         throw new HydroidException(e);
+      }
    }
 
    static HttpTransport newProxyTransport(HydroidConfiguration configuration) throws GeneralSecurityException, IOException {
       NetHttpTransport.Builder builder = new NetHttpTransport.Builder();
       builder.trustCertificates(GoogleUtils.getCertificateTrustStore());
       if(configuration.getProxyPort() > 0) {
-         builder.setProxy(new Proxy(Proxy.Type.HTTP, new InetSocketAddress("127.0.0.1", 3128)));
+         builder.setProxy(new Proxy(Proxy.Type.HTTP, new InetSocketAddress(configuration.getProxyHost(), configuration.getProxyPort())));
       }
       return builder.build();
    }
