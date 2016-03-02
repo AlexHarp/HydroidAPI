8b25eb290b104fe9e066aba56872d6003a017acd
scelton <elton.carneiro@ga.gov.au>
2016-03-02 14:13:43 +1100

    Removed explicit import of SchedulerConfiguration from the HydroidApplication class

--- src/main/java/au/gov/ga/hydroid/HydroidApplication.java
+++ src/main/java/au/gov/ga/hydroid/HydroidApplication.java
@@ -4,13 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.context.annotation.Import;
 
 /**
  * Created by u24529 on 3/02/2016.
  */
 @SpringBootApplication
-@Import({SchedulerConfiguration.class})
 public class HydroidApplication extends SpringBootServletInitializer {
 
    @Override
