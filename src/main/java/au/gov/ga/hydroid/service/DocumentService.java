31f085399651a95e9e9c77716a2f280c4612aa0f
Darren Reid <darren.reid@ga.gov.au>
2016-03-07 12:22:43 +1100

    Clean up redundant interface accessors.

--- src/main/java/au/gov/ga/hydroid/service/DocumentService.java
+++ src/main/java/au/gov/ga/hydroid/service/DocumentService.java
@@ -4,17 +4,12 @@ import au.gov.ga.hydroid.model.Document;
 
 import java.util.List;
 
-/**
- * Created by u24529 on 4/02/2016.
- */
 public interface DocumentService {
-
-   public List<Document> findAll();
-   public Document findByUrn(String urn);
-   public Document findByOrigin(String origin);
-   public void create(Document document);
-   public void deleteByUrn(String urn);
-   public void update(Document document);
-   public void clearAll();
-
+   List<Document> findAll();
+   Document findByUrn(String urn);
+   Document findByOrigin(String origin);
+   void create(Document document);
+   void deleteByUrn(String urn);
+   void update(Document document);
+   void clearAll();
 }
