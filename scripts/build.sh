3b6eb50e73d9e8ad8364ada0d1abdbc999af4d4b
Darren Reid <darren.reid@ga.gov.au>
2016-03-09 18:48:38 +1100

    Try fix sed syntax

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 # create common folder
 mkdir staging
-sed -i '\|__HYDROID_POSTGRES_PWD__|'"$HYDROID_POSTGRES_PWD"'|' src/main/java/resources/application.properties
+sed -i 's/__HYDROID_POSTGRES_PWD__/'"$HYDROID_POSTGRES_PWD"'/g' src/main/java/resources/application.properties
 # build hydroid api
 mvn clean package
 cp target/hydroid.jar staging/
