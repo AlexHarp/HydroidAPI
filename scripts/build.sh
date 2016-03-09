fcb03f045f1f87f1bd5bc5c83fd27173fb4551be
Darren Reid <darren.reid@ga.gov.au>
2016-03-09 17:39:14 +1100

    db variable replace creds

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 # create common folder
 mkdir staging
-
+sed -i -e 's@__HYDROID_POSTGRES_PWD__@'"$HYDROID_POSTGRES_PWD"'@' src/main/java/resources/application.properties
 # build hydroid api
 mvn clean package
 cp target/hydroid.jar staging/
