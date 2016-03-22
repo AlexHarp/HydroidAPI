baef11d9e3b389069e7b7a5e3f4f996cc7ee0a4e
Darren Reid <Layoric@users.noreply.github.com>
2016-03-22 15:54:39 +1100

    Update userdata_solr.sh
    Fix solr userdata script

--- scripts/userdata_solr.sh
+++ scripts/userdata_solr.sh
@@ -5,6 +5,7 @@ cd software
 wget https://www.apache.org/dist/lucene/solr/5.4.1/solr-5.4.1.zip
 unzip solr-5.4.1.zip
 aws s3 cp s3://hydroid/solr-cores/hydroid.zip /home/ec2-user/software/solr-5.4.1/server/solr/hydroid.zip
+cd /home/ec2-user/software/solr-5.4.1/server/solr
 unzip hydroid.zip
 cd /home/ec2-user/software/solr-5.4.1
 ./bin/solr restart
