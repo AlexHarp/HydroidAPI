aed565eddf97196adc1e83cd97b54cc4d52ae569
scelton <elton.carneiro@ga.gov.au>
2016-03-23 16:00:48 +1100

    Increased length of documents.title in the DB

--- src/main/scripts/create-schema.sql
+++ src/main/scripts/create-schema.sql
@@ -4,7 +4,7 @@ CREATE TABLE hydroid.documents (
     id bigserial PRIMARY KEY,
     origin varchar(500) NOT NULL,
     urn varchar(100) NULL,
-    title varchar(100) NULL,
+    title varchar(200) NULL,
     type varchar(20) NOT NULL,
     status varchar(20) NOT NULL,
     status_reason varchar(500) NULL,
