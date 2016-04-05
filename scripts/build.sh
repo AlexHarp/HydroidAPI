9ab336d7be5b8192c6dd7dbbe6903d527f450137
Darren Reid <darren.reid@ga.gov.au>
2016-04-05 12:44:46 +1000

    Test failures not failing travis builds
    Fix build process to show test failures in travis

--- scripts/build.sh
+++
@@ -1,7 +0,0 @@
-# create common folder
-mkdir staging
-sed -i 's/__HYDROID_POSTGRES_PWD__/'"$HYDROID_POSTGRES_PWD"'/g' ./src/main/resources/application.properties
-# build hydroid api
-mvn clean package
-cp target/hydroid.jar staging/
-cp google-vision.json staging/
