966e3189a9d4aa62da53edc9be2b4f0e2b2c4732
Darren Reid <darren.j.reid@gmail.com>
2016-04-18 17:23:02 +1000

    Built new indexing tool due to bug, updated scripts
    The changes made to the indexing tool are just switching a dependency
    that a) Shared package/class name and b) different version were imported
    in the parent vs a child pom. Everything would build fine but at runtime
    different versions of a type was being loaded than expected due to this
    collision. Update I made can be found at
    http://github.com/Layoric/stanbol/tree/0.12-index-tool-fix.

--- scripts/build-index.sh
+++ scripts/build-index.sh
@@ -3,8 +3,6 @@ java -jar -Xmx1g -XX:MaxPermSize=256M tools/org.apache.stanbol.entityhub.indexin
 cp pp_project_gapublicvocabssandbox.rdf indexing/resources/rdfdata/pp_project_gapublicvocabssandbox.rdf
 rm indexing/config/indexing.properties
 cp indexing.properties indexing/config/indexing.properties
-# The below command crashes on first try, no source available to fix, also doesn't exit.. timeout and then rerun.
-timeout 20s java -jar -Xmx1g -XX:MaxPermSize=256M tools/org.apache.stanbol.entityhub.indexing.genericrdf-0.12.1-SNAPSHOT.jar index
 java -jar -Xmx1g -XX:MaxPermSize=256M tools/org.apache.stanbol.entityhub.indexing.genericrdf-0.12.1-SNAPSHOT.jar index
 cp indexing/dist/GA.solrindex.zip staging/
 cp indexing/dist/org.apache.stanbol.data.site.GA-1.0.0.jar staging/
