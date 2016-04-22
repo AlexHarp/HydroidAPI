8b755199b28a1451ba9f9bee798daf8c6b97c03f
mikilovski <mitko.ilovski@gmail.com>
2016-04-22 16:12:10 +1000

    Finalised MenuController

--- src/main/java/au/gov/ga/hydroid/controller/MenuController.java
+++ src/main/java/au/gov/ga/hydroid/controller/MenuController.java
@@ -1,15 +1,11 @@
 package au.gov.ga.hydroid.controller;
 
 import au.gov.ga.hydroid.dto.MenuDTO;
-import au.gov.ga.hydroid.service.JenaService;
 import au.gov.ga.hydroid.utils.HydroidException;
-import au.gov.ga.hydroid.utils.IOUtils;
 import org.apache.jena.rdf.model.*;
+import org.apache.jena.vocabulary.RDF;
 import org.apache.jena.vocabulary.RDFS;
-import org.apache.jena.vocabulary.VCARD;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.jena.vocabulary.SKOS;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,47 +13,61 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by u24529 on 15/04/2016.
- */
 @RestController
 @RequestMapping("/menu")
 public class MenuController {
-    private Logger logger = LoggerFactory.getLogger(getClass());
-
-   @Autowired
-   private JenaService jenaService;
 
    @RequestMapping(value = "/hydroid", method = {RequestMethod.GET})
    public @ResponseBody ResponseEntity<List<MenuDTO>> enhance() {
 
-       MenuDTO menuItem;
-       List<MenuDTO> menu = new ArrayList<MenuDTO>();
+       MenuDTO menuItem, childMenuItem;
+       List<MenuDTO> menu = new ArrayList<>();
 
        try {
 
-           InputStream inputStream = getClass().getResourceAsStream("/hydroid.rdf");
-           String rdfContent =  new String(IOUtils.fromInputStreamToByteArray(inputStream));
-           List<Statement> statements = jenaService.parseRdf(rdfContent, "");
+           Model model = ModelFactory.createDefaultModel();
+           model.read("hydroid.rdf");
+           ResIterator resources = model.listResourcesWithProperty(RDF.type);
 
-           for(Statement statement : statements) {
-               menuItem = new MenuDTO();
+           while (resources.hasNext()) {
+               Resource parentRes = resources.nextResource();
 
-               if (statement.getPredicate().toString().toUpperCase().contains("LABEL")) {
-                   menuItem.setNodeURI(statement.getSubject().getURI());
-                   menuItem.setNodeLabel(statement.getString());
+               if (parentRes.hasProperty(RDFS.label)) {
+                   Statement resStmt = parentRes.getProperty(RDFS.label);
+
+                   menuItem = new MenuDTO();
+                   menuItem.setNodeLabel(resStmt.getString());
+                   menuItem.setNodeURI(resStmt.getSubject().getURI());
+
+                   List<MenuDTO> childMenu = new ArrayList<>();
+                   ResIterator childResources = model.listResourcesWithProperty(SKOS.topConceptOf);
+
+                   while (childResources.hasNext()) {
+                       Resource childRes = childResources.next();
+
+                       String childURI = childRes.getProperty(SKOS.topConceptOf).getObject().toString();
+                       String parentURI = resStmt.getSubject().getURI();
+
+                       if (childURI.equals(parentURI)) {
+                           Statement childResStmt = childRes.getProperty(SKOS.prefLabel);
+                           childMenuItem = new MenuDTO();
+                           childMenuItem.setNodeLabel(childResStmt.getString());
+                           childMenuItem.setNodeURI(childResStmt.getSubject().getURI());
+                           childMenu.add(childMenuItem);
+                       }
+                   }
+                   menuItem.setChildren(childMenu);
                    menu.add(menuItem);
                }
            }
-
        } catch (Exception e) {
            throw new HydroidException(e);
        }
 
-      return new ResponseEntity<>(menu, HttpStatus.OK);
+       System.out.println("Done!");
+       return new ResponseEntity<>(menu, HttpStatus.OK);
    }
 }
